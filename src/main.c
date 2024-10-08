4621df7e96cfbab9e03186ba5fcec6479b85709c
Christoph Ungricht <christoph.ungricht@outlook.com>
2024-10-08 10:51:38 +0200

    feat: Implemented forward propagation.
    Signed-off-by: Christoph Ungricht <christoph.ungricht@outlook.com>

--- src/main.c
+++ src/main.c
@@ -7,8 +7,11 @@
 #include <time.h>
 
 #define PRINTF printf
-#define WIN_HEIGHT 28
-#define WIN_WIDTH 28
+#define NUM_LAYERS 3
+#define NUM_INPUTS (28 * 28)
+#define NUM_OUTPUTS 10
+#define WIN_HEIGHT (NUM_INPUTS / 2)
+#define WIN_WIDTH WIN_HEIGHT
 #define TARGET_FPS 60
 #define FLOAT double
 #define MALLOC malloc
@@ -25,6 +28,8 @@ static bool random_init = false;
   } while (0)
 #define EXIT(c) exit(c)
 
+#define IDX(i, j, m) ((i) * (m) + (j))
+
 typedef struct {
   size_t num_layers;
   size_t *layer_sizes;
@@ -38,7 +43,7 @@ typedef Network_t *Network;
 static FLOAT *randn(size_t n) {
   INIT_RAND();
   size_t m = n + n % 2;
-  FLOAT *values = (FLOAT *)CALLOC(m, sizeof(FLOAT));
+  FLOAT *values = (FLOAT *)CALLOC(m, sizeof(values[0]));
   if (!values) {
     TraceLog(LOG_FATAL, "Could not create random array, out of memory");
     EXIT(1);
@@ -59,7 +64,7 @@ static FLOAT *randn(size_t n) {
 
 Network network_create(size_t *sizes, size_t num_layers) {
   Network network = MALLOC(sizeof(*network));
-  size_t *layer_sizes = MALLOC(num_layers * sizeof(size_t));
+  size_t *layer_sizes = MALLOC(num_layers * sizeof(layer_sizes[0]));
   FLOAT **biases = MALLOC((num_layers - 1) * sizeof(FLOAT));
   FLOAT **weights = MALLOC((num_layers - 1) * sizeof(FLOAT));
   if (!network || !layer_sizes || !biases || !weights) {
@@ -95,41 +100,71 @@ void network_free(Network network) {
   free(network);
 }
 
-static inline size_t weight_index(size_t i, size_t j, size_t m) {
-  return i * m + j;
-}
-
 void network_print(Network network) {
 
   PRINTF("Network: \n");
   PRINTF("Number of layers: %zu\n", network->num_layers);
   PRINTF("Layer sizes: [ ");
-  for (size_t i = 0; i < network->num_layers; ++i) {
-    PRINTF("%zu ", network->layer_sizes[i]);
+  for (size_t l = 0; l < network->num_layers; ++l) {
+    PRINTF("%zu ", network->layer_sizes[l]);
   }
   PRINTF("]\n");
 
-  for (size_t i = 0; i < network->num_layers - 1; ++i) {
-    PRINTF("Biases %zu: [ ", i);
-    for (size_t j = 0; j < network->layer_sizes[i + 1]; ++j) {
-      PRINTF("%f ", network->biases[i][j]);
+  for (size_t l = 0; l < network->num_layers - 1; ++l) {
+    PRINTF("Biases %zu: [ ", l);
+    for (size_t i = 0; i < network->layer_sizes[l + 1]; ++i) {
+      PRINTF("%f ", network->biases[l][i]);
     }
     PRINTF("]\n");
   }
 
   // Print the matrix weights
-  for (size_t i = 0; i < network->num_layers - 1; ++i) {
-    PRINTF("Weights %zu:\n", i);
-    size_t n = network->layer_sizes[i + 1];
-    size_t m = network->layer_sizes[i];
-    for (size_t j = 0; j < n; ++j) {
-      for (size_t k = 0; k < m; ++k) {
-        PRINTF("%f ", network->weights[i][weight_index(j, k, m)]);
+  for (size_t l = 0; l < network->num_layers - 1; ++l) {
+    PRINTF("Weights %zu:\n", l);
+    size_t n = network->layer_sizes[l + 1];
+    size_t m = network->layer_sizes[l];
+    for (size_t i = 0; i < n; ++i) {
+      for (size_t j = 0; j < m; ++j) {
+        PRINTF("%f ", network->weights[l][IDX(i, j, m)]);
       }
       PRINTF("\n");
     }
     PRINTF("\n");
   }
+}
+
+static inline void sigmoid(FLOAT *z, FLOAT *out, size_t len) {
+  for (size_t i = 0; i < len; ++i) {
+    out[i] = 1 / (1 + exp(-z[i]));
+  }
+}
+
+static inline void dot_add(FLOAT *W, FLOAT *x, FLOAT *b, FLOAT *out, size_t n,
+                           size_t m) {
+  for (size_t i = 0; i < n; ++i) {
+    FLOAT tmp = 0;
+    for (size_t j = 0; j < m; ++j) {
+      tmp += W[IDX(i, j, m)] * x[j];
+    }
+    out[i] = tmp + b[i];
+  }
+}
+
+void network_feedforward(Network network, FLOAT *input, FLOAT *output) {
+  FLOAT *x = input;
+  FLOAT *a = NULL;
+  size_t n = 0;
+  for (size_t l = 0; l < network->num_layers - 1; ++l) {
+    n = network->layer_sizes[l + 1];
+    size_t m = network->layer_sizes[l];
+    a = alloca(network->layer_sizes[l + 1] * sizeof(a[0]));
+    FLOAT *W = network->weights[l];
+    FLOAT *b = network->biases[l];
+    dot_add(W, x, b, a, n, m);
+    sigmoid(a, a, n); // Inplace
+    x = a;
+  }
+  memcpy(output, a, n * sizeof(output[0]));
 }
 
 /**/
@@ -146,13 +181,26 @@ void network_print(Network network) {
 /*   } */
 /* } */
 
-#define NUM_LAYERS 3
+void print_output_layer(FLOAT *out, size_t n) {
+  PRINTF("---------------- OUTPUT PER NEURON ----------------\n");
+  for (size_t i = 0; i < n; ++i) {
+    PRINTF("%lu => %f \n", i, out[i]);
+  }
+  PRINTF("---------------------------------------------------\n");
+}
 
 int main(void) {
-  size_t layer_sizes[NUM_LAYERS] = {2, 3, 1};
+  size_t layer_sizes[NUM_LAYERS] = {NUM_INPUTS, 100, NUM_OUTPUTS};
   Network network = network_create(layer_sizes, NUM_LAYERS);
 
-  network_print(network);
+  /* network_print(network); */
+
+  FLOAT out[NUM_OUTPUTS] = {0};
+  FLOAT *in = randn(NUM_INPUTS);
+
+  network_feedforward(network, in, &out[0]);
+
+  print_output_layer(out, NUM_OUTPUTS);
 
   network_free(network);
 }
